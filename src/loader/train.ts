a153ce96889a9867545c778facab3e467004e7c6
mjkim1225 <01026599478a@gmail.com>
2023-07-18 14:56:29 +0900

    2,3호선 데이터 추가

--- src/loader/train.ts
+++ src/loader/train.ts
@@ -57,7 +57,7 @@ const getVelocity = (accDistance: number, noAccDistance: number, elapsedSec: num
 }
 
 export default (railwaysInfo: RailwayInfo[], timetablesInfo: TimetablesInfo[]) => {
-    const lines = ["line1", "line2", "line3", "line4", "line5", "line6", "line7", "line8"]
+    const lines = ["line1", "line2", "line7"]
 
     const dataSet: DataSet[] = [];
 
